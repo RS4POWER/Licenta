2c198100d35c9a5e92443703d65c891cd3684874
RS4POWER <alexandru.regulas@student.upt.ro>
2024-04-30 16:42:00 +0300

    adaugare interogari baza date si citire data sistem

--- CitireContoare/app/src/main/java/com/example/citirecontoare/HouseDetailsActivity.java
+++ CitireContoare/app/src/main/java/com/example/citirecontoare/HouseDetailsActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -14,8 +16,13 @@ import android.widget.Toast;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class HouseDetailsActivity extends AppCompatActivity {
@@ -24,9 +31,18 @@ public class HouseDetailsActivity extends AppCompatActivity {
     private TextView ownerNameTextView, houseNumberTextView;
     private EditText marcaEditText, seriaEditText, diametruEditText, dataInstalareEditText,stareApometruEditText,consumMcEditText,datacitiriiEditText;
     private boolean isInEditMode = false;
-    private ImageButton manualModifyButton;
+    private ImageButton backButton,manualModifyButton,  previousYearButton, nextYearButton;
+    private  Button nextMonthButton,previousMonthButton;
+
 
     // Adaug restul EditText-urilor pentru consum, stare, data citirii, etc.
+
+    private int currentYear;
+    private int currentMonth;
+    private Long houseNumber;
+
+    public  String[] monthNames = {"Ianuarie", "Februarie", "Martie", "Aprilie", "Mai", "Iunie",
+            "Iulie", "August", "Septembrie", "Octombrie", "Noiembrie", "Decembrie"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +61,67 @@ public class HouseDetailsActivity extends AppCompatActivity {
         stareApometruEditText = findViewById(R.id.stareApometruEditText);
         consumMcEditText = findViewById(R.id.consumMcEditText);
         datacitiriiEditText = findViewById(R.id.datacitiriiEditText);
-
+        backButton = findViewById(R.id.backButton);
+        ImageButton previousYearButton = findViewById(R.id.previousYearButton);
+        ImageButton nextYearButton = findViewById(R.id.nextYearButton);
+        Button previousMonthButton = findViewById(R.id.previousMonthButton);
+        Button nextMonthButton = findViewById(R.id.nextMonthButton);
 
         // Inițializează restul EditText-urilor aici
+
+        currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        currentMonth = Calendar.getInstance().get(Calendar.MONTH);
+
+
+
+        updateDateDisplay();
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Închide activitatea curentă
+            }
+
+        });
+
+        previousYearButton.setOnClickListener(v -> {
+            currentYear--;
+            updateDateDisplay();
+            loadApometruDetails(houseNumber, currentYear, currentMonth);
+            loadHouseDetails(houseNumber);
+        });
+
+        nextYearButton.setOnClickListener(v -> {
+            currentYear++;
+            updateDateDisplay();
+            loadApometruDetails(houseNumber, currentYear, currentMonth);
+            loadHouseDetails(houseNumber);
+        });
+
+        previousMonthButton.setOnClickListener(v -> {
+            if (currentMonth > 0) {
+                currentMonth--;
+            } else {
+                currentMonth = 11;
+                currentYear--;
+            }
+            updateDateDisplay();
+            loadApometruDetails(houseNumber, currentYear, currentMonth);
+            loadHouseDetails(houseNumber);
+        });
+
+        nextMonthButton.setOnClickListener(v -> {
+            if (currentMonth < 11) {
+                currentMonth++;
+            } else {
+                currentMonth = 0;
+                currentYear++;
+            }
+            updateDateDisplay();
+            loadApometruDetails(houseNumber, currentYear, currentMonth);
+            loadHouseDetails(houseNumber);
+        });
+
+
         toggleEditMode(false);
         manualModifyButton.setOnClickListener(v -> {
             isInEditMode = !isInEditMode;
@@ -58,6 +132,7 @@ public class HouseDetailsActivity extends AppCompatActivity {
             } else {
                 manualModifyButton.setBackgroundResource(R.drawable.baseline_edit_note_24);
                 saveHouseDetails(); // Salvează datele când ieși din modul de editarehoue
+                saveApometruDetails();
             }
             toggleEditMode(isInEditMode);
         });
@@ -65,7 +140,7 @@ public class HouseDetailsActivity extends AppCompatActivity {
 
         // Obține numărul casei și numele proprietarului din intent
         Intent intent = getIntent();
-        Long houseNumber = intent.getLongExtra("HOUSE_NUMBER", -1);
+         houseNumber = intent.getLongExtra("HOUSE_NUMBER", -1);
         if(houseNumber == -1) {
             Toast.makeText(this, "Numărul casei nu a fost transmis corect.", Toast.LENGTH_SHORT).show();
             finish(); // Închide activitatea dacă numărul casei nu este valid
@@ -76,6 +151,19 @@ public class HouseDetailsActivity extends AppCompatActivity {
 
         // Aduce detalii specifice casei din Firestore
         loadHouseDetails(houseNumber);
+        loadApometruDetails(houseNumber, currentYear, currentMonth);
+
+
+
+        intent = getIntent();
+         houseNumber = intent.getLongExtra("HOUSE_NUMBER", -1);
+
+        if(houseNumber != -1) {
+            loadApometruDetails(houseNumber, currentYear, currentMonth);
+        } else {
+            Toast.makeText(this, "Numărul casei nu a fost transmis corect.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
     private void loadHouseDetails(Long houseNumber) {
@@ -101,6 +189,43 @@ public class HouseDetailsActivity extends AppCompatActivity {
             Toast.makeText(this, "Eroare la încărcarea detaliilor casei.", Toast.LENGTH_SHORT).show();
         });
     }
+
+    private void loadApometruDetails(Long houseNumber, int year, int month) {
+        String monthName = monthNames[month];
+        DocumentReference monthRef = db.collection("zones")
+                .document("Iacobini")
+                .collection("numereCasa")
+                .document("Numarul " + houseNumber)
+                .collection("consumApa")
+                .document(String.valueOf(year))
+                .collection("lunile")
+                .document(monthName);
+
+        monthRef.get().addOnSuccessListener(documentSnapshot -> {
+            if (documentSnapshot.exists()) {
+                // Verifică și tratează 'Starea Apometrului' ca număr
+                Long stareaApometrului = documentSnapshot.getLong("Starea Apometrului");
+                if (stareaApometrului != null) {
+                    stareApometruEditText.setText(String.valueOf(stareaApometrului)); // Convertim la String
+                } else {
+                    stareApometruEditText.setText("Nedefinit");
+                }
+
+                // Procesează restul datelor ca string-uri
+                consumMcEditText.setText(String.valueOf(documentSnapshot.getLong("Consumatia mc"))); // Convertim numărul la String
+                datacitiriiEditText.setText(documentSnapshot.getString("Data citire"));
+
+            } else {
+                Toast.makeText(this, "Nu există înregistrări pentru " + monthName + " " + year, Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(e -> {
+            Toast.makeText(this, "Eroare la încărcarea datelor pentru " + monthName + " " + year, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+
+
+
 
     private void toggleEditMode(boolean isInEditMode) {
         marcaEditText.setEnabled(isInEditMode);
@@ -155,20 +280,61 @@ public class HouseDetailsActivity extends AppCompatActivity {
     }
 
 
+    private void saveApometruDetails() {
+        Long houseNumber = getIntent().getLongExtra("HOUSE_NUMBER", -1);
+        String stareApometru = stareApometruEditText.getText().toString();
+        String consumMc = consumMcEditText.getText().toString();
+        String dataCitirii = datacitiriiEditText.getText().toString();
 
-    // Metoda pentru inițializarea și setarea acțiunilor butoanelor, dacă este necesar
-    // private void setupButtons() {
-    //     // Inițializează butoanele și setează OnClickListener pentru fiecare
-    // }
+        if (!isValidNumber(consumMc)) {
+            Toast.makeText(this, "Consumația trebuie să fie un număr valid.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        double consumValue = Double.parseDouble(consumMc);
+        long consumInt = (long) consumValue;
+
+        DocumentReference monthRef = db.collection("zones")
+                .document("Iacobini")
+                .collection("numereCasa")
+                .document("Numarul " + houseNumber)
+                .collection("consumApa")
+                .document(String.valueOf(currentYear))
+                .collection("lunile")
+                .document(monthNames[currentMonth]);
+
+        Map<String, Object> apometruDetails = new HashMap<>();
+        apometruDetails.put("Starea Apometrului", Long.parseLong(stareApometru));
+        apometruDetails.put("Consumatia mc", consumInt);
+        apometruDetails.put("Data citire", dataCitirii);
+
+        monthRef.update(apometruDetails)
+                .addOnSuccessListener(aVoid -> {
+                    isInEditMode = false;
+                    toggleEditMode(false);
+                })
+                .addOnFailureListener(e -> Toast.makeText(HouseDetailsActivity.this, "Eroare la actualizarea datelor apometrului.", Toast.LENGTH_SHORT).show());
+    }
+
 
     private boolean isValidNumber(String numberStr) {
         try {
-            Long.parseLong(numberStr);
+            Double.parseDouble(numberStr);
             return true;
         } catch (NumberFormatException e) {
             return false;
         }
     }
+
+
+    private void updateDateDisplay() {
+        String monthName = monthNames[currentMonth];
+        String displayText = monthName + ", " + currentYear;
+        TextView dateTextView = findViewById(R.id.dateTextView);
+        dateTextView.setText(displayText);
+    }
+
+
 }
 
 
